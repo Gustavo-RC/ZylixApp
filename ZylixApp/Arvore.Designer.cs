fb91a89239a1d3a83d58f3b81444ef7c03c29deb
Gustavo <gu171994@gmail.com>
2020-05-12 00:21:23 -0300

    Versão Inicial

---
+++ ZylixApp/Arvore.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ZylixApp
+{
+    partial class Arvore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode105 = new System.Windows.Forms.TreeNode("000 - System Layout");
+            System.Windows.Forms.TreeNode treeNode106 = new System.Windows.Forms.TreeNode("100 - Pulse Inputs");
+            System.Windows.Forms.TreeNode treeNode107 = new System.Windows.Forms.TreeNode("200 - Pulse Outputs");
+            System.Windows.Forms.TreeNode treeNode108 = new System.Windows.Forms.TreeNode("300 - Digital Inputs");
+            System.Windows.Forms.TreeNode treeNode109 = new System.Windows.Forms.TreeNode("500 - Digital Outputs");
+            System.Windows.Forms.TreeNode treeNode110 = new System.Windows.Forms.TreeNode("900 - Analog I/O");
+            System.Windows.Forms.TreeNode treeNode111 = new System.Windows.Forms.TreeNode("Config Directory", new System.Windows.Forms.TreeNode[] {
+            treeNode105,
+            treeNode106,
+            treeNode107,
+            treeNode108,
+            treeNode109,
+            treeNode110});
+            System.Windows.Forms.TreeNode treeNode112 = new System.Windows.Forms.TreeNode("Security Directory");
+            System.Windows.Forms.TreeNode treeNode113 = new System.Windows.Forms.TreeNode("100 - General Purpose");
+            System.Windows.Forms.TreeNode treeNode114 = new System.Windows.Forms.TreeNode("200 - Flow Control");
+            System.Windows.Forms.TreeNode treeNode115 = new System.Windows.Forms.TreeNode("300 - Volume Accuracy");
+            System.Windows.Forms.TreeNode treeNode116 = new System.Windows.Forms.TreeNode("400 - Temperature/Density");
+            System.Windows.Forms.TreeNode treeNode117 = new System.Windows.Forms.TreeNode("500 - Pressure");
+            System.Windows.Forms.TreeNode treeNode118 = new System.Windows.Forms.TreeNode("600 - Default Alarms");
+            System.Windows.Forms.TreeNode treeNode119 = new System.Windows.Forms.TreeNode("700 - Communications");
+            System.Windows.Forms.TreeNode treeNode120 = new System.Windows.Forms.TreeNode("800 - Additives (common)");
+            System.Windows.Forms.TreeNode treeNode121 = new System.Windows.Forms.TreeNode("1600 - Board Addresses");
+            System.Windows.Forms.TreeNode treeNode122 = new System.Windows.Forms.TreeNode("System Directory", new System.Windows.Forms.TreeNode[] {
+            treeNode112,
+            treeNode113,
+            treeNode114,
+            treeNode115,
+            treeNode116,
+            treeNode117,
+            treeNode118,
+            treeNode119,
+            treeNode120,
+            treeNode121});
+            System.Windows.Forms.TreeNode treeNode123 = new System.Windows.Forms.TreeNode("Arm 1");
+            System.Windows.Forms.TreeNode treeNode124 = new System.Windows.Forms.TreeNode("Arm 2");
+            System.Windows.Forms.TreeNode treeNode125 = new System.Windows.Forms.TreeNode("Arm 3");
+            System.Windows.Forms.TreeNode treeNode126 = new System.Windows.Forms.TreeNode("Arm 4");
+            System.Windows.Forms.TreeNode treeNode127 = new System.Windows.Forms.TreeNode("Arm 5");
+            System.Windows.Forms.TreeNode treeNode128 = new System.Windows.Forms.TreeNode("Arm 6");
+            System.Windows.Forms.TreeNode treeNode129 = new System.Windows.Forms.TreeNode("Recipe Directory");
+            System.Windows.Forms.TreeNode treeNode130 = new System.Windows.Forms.TreeNode("Markings & Notices");
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(2, 76);
+            this.treeView1.Name = "treeView1";
+            treeNode105.Name = "000 - System Layout";
+            treeNode105.Text = "000 - System Layout";
+            treeNode106.Name = "100 - Pulse Inputs";
+            treeNode106.Text = "100 - Pulse Inputs";
+            treeNode107.Name = "200 - Pulse Outputs";
+            treeNode107.Text = "200 - Pulse Outputs";
+            treeNode108.Name = "300 - Digital Inputs";
+            treeNode108.Text = "300 - Digital Inputs";
+            treeNode109.Name = "500 - Digital Outputs";
+            treeNode109.Text = "500 - Digital Outputs";
+            treeNode110.Name = "900 - Analog I/O";
+            treeNode110.Text = "900 - Analog I/O";
+            treeNode111.Name = "Config Directory";
+            treeNode111.Text = "Config Directory";
+            treeNode112.Name = "Security Directory";
+            treeNode112.Text = "Security Directory";
+            treeNode113.Name = "100 - General Purpose";
+            treeNode113.Text = "100 - General Purpose";
+            treeNode114.Name = "200 - Flow Control";
+            treeNode114.Text = "200 - Flow Control";
+            treeNode115.Name = "300 - Volume Accuracy";
+            treeNode115.Text = "300 - Volume Accuracy";
+            treeNode116.Name = "400 - Temperature/Density";
+            treeNode116.Text = "400 - Temperature/Density";
+            treeNode117.Name = "500 - Pressure";
+            treeNode117.Text = "500 - Pressure";
+            treeNode118.Name = "600 - Default Alarms";
+            treeNode118.Text = "600 - Default Alarms";
+            treeNode119.Name = "700 - Communications";
+            treeNode119.Text = "700 - Communications";
+            treeNode120.Name = "800 - Additives (common)";
+            treeNode120.Text = "800 - Additives (common)";
+            treeNode121.Name = "1600 - Board Addresses";
+            treeNode121.Text = "1600 - Board Addresses";
+            treeNode122.Name = "System Directory";
+            treeNode122.Text = "System Directory";
+            treeNode123.Name = "Arm 1";
+            treeNode123.Text = "Arm 1";
+            treeNode124.Name = "Arm 2";
+            treeNode124.Text = "Arm 2";
+            treeNode125.Name = "Arm 3";
+            treeNode125.Text = "Arm 3";
+            treeNode126.Name = "Arm 4";
+            treeNode126.Text = "Arm 4";
+            treeNode127.Name = "Arm 5";
+            treeNode127.Text = "Arm 5";
+            treeNode128.Name = "Arm 6";
+            treeNode128.Text = "Arm 6";
+            treeNode129.Name = "Recipe Directory";
+            treeNode129.Text = "Recipe Directory";
+            treeNode130.Name = "Markings & Notices";
+            treeNode130.Text = "Markings & Notices";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode111,
+            treeNode122,
+            treeNode123,
+            treeNode124,
+            treeNode125,
+            treeNode126,
+            treeNode127,
+            treeNode128,
+            treeNode129,
+            treeNode130});
+            this.treeView1.Size = new System.Drawing.Size(190, 619);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.editarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1133, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.novoToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(91, 29);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salvarToolStripMenuItem});
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(73, 29);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.novoToolStripMenuItem.Text = "Novo";
+            // 
+            // salvarToolStripMenuItem
+            // 
+            this.salvarToolStripMenuItem.Name = "salvarToolStripMenuItem";
+            this.salvarToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.salvarToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.salvarToolStripMenuItem.Text = "Salvar";
+            this.salvarToolStripMenuItem.Click += new System.EventHandler(this.salvarToolStripMenuItem_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.SelectedPath = "C:\\Users\\gusta\\Desktop";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // Arvore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 707);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Arvore";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Arvore_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
+
---
+++ ZylixApp/Arvore.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ZylixApp
+{
+    public partial class Arvore : Form
+    {
+        public Arvore()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Arvore_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void salvarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Arquivo texto | *.txt";
+            sfd.ShowDialog();
+
+            if (string.IsNullOrEmpty(sfd.FileName) == false)
+            {
+                using (StreamWriter writer = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+                {
+
+                }
+            }
+        }
+    }
+}
